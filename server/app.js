e7adb345fec1882e36cbc8452cdc74e1612597af
Sam Moss <sam.moss321@gmail.com>
2015-11-23 10:09:16 -0600

    client server communication

--- server/app.js
+++ server/app.js
@@ -1,3 +1,20 @@
 /**
  * Created by samuelmoss on 11/23/15.
  */
+var express = require('express');
+var app = express();
+var pg = require('pg');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+app.set('port', process.env.PORT || 5000);
+
+app.get('/', function(req,res){
+    var file = req.params[0] || './assets/views/index.html';
+    res.sendFile(path.join(__dirname,'./Public',file))
+});
+
+app.listen(app.get('port'),function(){
+     console.log("d(=^.^=)b Listening on port :", app.get('port'));
+    }
+);
